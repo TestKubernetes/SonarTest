16ca792ada21642477384deb99dec25e1ae573c7
Aparna <Aparna@DESKTOP-7JFOIO2>
2019-12-18 21:40:39 +0530

    test

--- SonarTestApp/src/code/SonarTest.java
+++ SonarTestApp/src/code/SonarTest.java
@@ -1,36 +1,17 @@
 package code;
 
 
-/*
- * public class SonarTest {
- * 
- * public static void main(String[] args) {
- * 
- * System.out.println("Hello world");
- * System.out.println("Hello world new 1 helps");
- * 
- * //System.out.print(false);
- * 
- * 
- * 
- * }
- * 
- * }
- */
-
 public class SonarTest {
-    public int add(int a, int b) {
-        return a + b;
-    }
-    public int subtract(int a, int b) {
-        return a - b;
-    }
-    public int multiply(int a, int b) {
-        return a * b;
-    }
-    public int divide(int a, int b) {
-        return a / b;
-    }
-    
-  //  System.out.println("==============================");
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello world");
+		System.out.println("Hello world new 1 helps");
+		
+		//System.out.print(false);
+		
+		
+		
+	}
+	
 }
