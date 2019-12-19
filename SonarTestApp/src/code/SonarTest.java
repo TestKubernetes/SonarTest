11b9aeda8110a45e5b09891a120fb6bc332a70f3
Aparna <Aparna@DESKTOP-7JFOIO2>
2019-12-19 18:20:46 +0530

    test

--- SonarTestApp/src/code/SonarTest.java
+++ SonarTestApp/src/code/SonarTest.java
@@ -6,7 +6,7 @@ public class SonarTest {
 	public static void main(String[] args) {
 		
 		System.out.println("Hello world");
-		System.out.println("Hello world new 1 helps");
+		System.out.println("Hello world new ");
 		
 		}
 	
