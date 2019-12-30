18dba290dc7c312e7806edd89efadae4579780c2
Aparna <Aparna@host.docker.internal>
2019-12-31 01:27:36 +0530

    test

--- SonarTestApp/src/code/SonarTest.java
+++ SonarTestApp/src/code/SonarTest.java
@@ -1,16 +1,10 @@
 package code;
 
-
 public class SonarTest {
 
-	public static void main(String[] args) {
-		
-		
-		System.out.println("Hello world");
-		System.out.println("Hello world new ");
-		System.out.println("Hello world");
-		System.out.println("Hello world new ");
-		
+	public static void main(String[] args) {		
+		System.err.println("Hello world");	
+		skldsjkd
 		}
 	
 }
