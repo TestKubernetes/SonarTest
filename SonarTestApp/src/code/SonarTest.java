10cd3c34e66cc469d81de517ff9662c759606950
Aparna <Aparna@host.docker.internal>
2020-01-07 15:39:07 +0530

    test

--- SonarTestApp/src/code/SonarTest.java
+++ SonarTestApp/src/code/SonarTest.java
@@ -3,9 +3,12 @@ package code;
 public class SonarTest {
 
 	public static void main(String[] args) {		
-		System.err.println("Hello world");	
+		System.out.println("Hello world");	
 		
-		
+		String[] strArray = {"One","Two","Three"};
+		 
+		for(String str : strArray)
+		    System.out.println(str);
 		
 		}
 	
