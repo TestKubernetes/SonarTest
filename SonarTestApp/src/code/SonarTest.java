84cc94102105d2c4f62fa573959907a5743a1fb2
Aparna <Aparna@host.docker.internal>
2020-01-07 20:30:57 +0530

    test

--- SonarTestApp/src/code/SonarTest.java
+++ SonarTestApp/src/code/SonarTest.java
@@ -5,23 +5,6 @@ public class SonarTest {
 	public static void main(String[] args) {		
 		System.out.println("Hello world");	
 		
-		String[] strArray = {"One","Two","Three"};
-		 
-		for(String str : strArray)
-		    System.out.println(str);
-		System.out.println("Hello world");	
-		
-		djsldj
-		
-		
-		if(1===2)
-			System.out.println("equal");
-		String password="1234";
-		String password1="12345";
-		sadjdlksaj
-		
-	
-		
-		}
+				}
 	
 }
