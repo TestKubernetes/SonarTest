7fed82d03789efc978efb3c363e9f7e33b341abb
Aparna <Aparna@host.docker.internal>
2020-01-07 21:46:15 +0530

    test

--- SonarTestApp/src/code/SonarTest.java
+++ SonarTestApp/src/code/SonarTest.java
@@ -4,16 +4,6 @@ public class SonarTest {
 
 	public static void main(String[] args) {		
 		System.out.println("Hello world");
-		System.out.println("Hello world");
-		
-		System.out.println("Hello world");
-		//dks;l
-		//fkds
-		//ffe
-		//ff
-		System.out.println("Hello world");
-		
-		
 				}
 	
 }
