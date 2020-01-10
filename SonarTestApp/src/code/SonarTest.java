7a02c9e7518242a372ba135aa638fedbcec85e84
Aparna <Aparna@9.85.110.36>
2020-01-10 21:51:27 +0530

    test

--- SonarTestApp/src/code/SonarTest.java
+++ SonarTestApp/src/code/SonarTest.java
@@ -5,9 +5,6 @@ class SonarTest {
 	public static void main(String[] args) {		
 System.out.println("ddddddddddd");
 System.out.println();
-//sssssssssss
-//dkkkkkkkkk
-//String password="password";
 	}
 	
 	
