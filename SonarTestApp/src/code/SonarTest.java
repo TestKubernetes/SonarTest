a2ebd1703bee8c6581624ed56ad0948c1fc02ea1
Aparna <Aparna@9.85.117.40>
2020-01-10 20:29:40 +0530

    test

--- SonarTestApp/src/code/SonarTest.java
+++ SonarTestApp/src/code/SonarTest.java
@@ -4,11 +4,9 @@ class SonarTest {
 
 	public static void main(String[] args) {		
 		System.out.println("Hello world");		
-		System.out.println("Hello world");	
-		System.out.println("Hello world");	
-		//dks;lad
-		//fkd;lsf
-
+		
+//dkkkkkk
+		//dkkkkkkkkkk
 	}
 	
 }
