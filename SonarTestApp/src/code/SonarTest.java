c57b596d3d54d51a334dda192483dbe0fa80eefa
aparna <aparnaondwar@gmail.com>
2020-02-14 20:34:13 +0530

    test

--- SonarTestApp/src/code/SonarTest.java
+++ SonarTestApp/src/code/SonarTest.java
@@ -5,7 +5,7 @@ class SonarTest {
 	public static void main(String[] args) {		
  System.out.println("Hello World");
 
- //String password = "password";
+ String password = "password";
  System.out.println("Hello World1");
 	}
 	
